20019307ffa973beaf3d9e2b26b35efb7c7a2508
Kiridia <evgenev.kostya1337@gmail.com>
2022-05-06 23:41:30 +0300

    html + btn + js

---
+++ script.js
@@ -0,0 +1,28 @@
+const videoElement = document.getElementById('video');
+const button = document.getElementById('button');
+
+// Promt to select media stream, pass to video element, than display
+
+async function selectMediaStream() {
+  try {
+    const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+    videoElement.srcObject = mediaStream;
+    videoElement.onloadmetodata = () => {
+      videoElement.play();
+    }
+  } catch (e) {
+    //Catch Error Here
+  }
+}
+
+button.addEventListener('click', async () => {
+  //Disable Button
+  button.disabled = true;
+  // Start Picture In Picture
+  await videoElement.requestPictureInPicture();
+  // Reset Button
+  button.disabled = false;
+})
+
+// On Load
+selectMediaStream();
